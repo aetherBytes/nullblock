f88a7c40e95983bed5b32774458961aece6fd7d5
aetherBytes <sage.software42@gmail.com>
2024-02-18 09:05:37 -0700

    chore: skeleton files VITE

---
+++ src/common/services/route-manager.ts
@@ -0,0 +1,35 @@
+import { Manager } from '@lomray/react-route-manager';
+
+/**
+ * Application URL manager
+ */
+const manager = new Manager({
+  routes: {
+    home: {
+      url: '/',
+    },
+    details: {
+      url: '/details',
+      children: {
+        user: {
+          url: '/user/:id',
+          params: { id: '' },
+        },
+      },
+    },
+    errorBoundary: {
+      url: '/error-boundary',
+    },
+    nestedSuspense: {
+      url: '/nested-suspense',
+    },
+    redirect: {
+      url: '/redirect-demo',
+    },
+    notLazy: {
+      url: '/not-lazy',
+    },
+  },
+});
+
+export default manager;
