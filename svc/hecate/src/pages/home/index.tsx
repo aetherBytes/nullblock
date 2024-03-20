ba7c2e8fb510492bb225cda4dfa2c85c2920a555
aetherBytes <sage.software42@gmail.com>
2024-03-19 22:05:21 -0600

    add chat box

--- svc/hecate/src/pages/home/index.tsx
+++ svc/hecate/src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import Moxi from '@components/moxi/moxi'; // Adjust the import path as needed
 import Echo from '@components/echo/echo'; // Adjust the import path as needed
@@ -7,6 +6,7 @@ import styles_buttons from '@components/index-button-wrapper/styles.module.scss'
 import menu_white from '@assets/images/menu_echo.png'; // Adjust the import path as needed
 import ButtonWrapper from '@components/index-button-wrapper/button-wrapper'; // Adjust the import path as needed
 import styles from './styles.module.scss'; // Ensure the path is correct for your styles
+import ChatInput from '@components/chat-input/chat-input'; // Ensure the import path is correct
 
 const Home = () => {
   const [isEchoVisible, setIsEchoVisible] = useState(true);
@@ -16,8 +16,9 @@ const Home = () => {
   return (
     <div className={styles.backgroundImage}>
       <Moxi toggleEchoVisibility={toggleEchoVisibility} />
+      <ChatInput />
       <div className={styles_buttons.buttonsContainer}>
-        <AppMenu toggleEchoVisibility={toggleEchoVisibility} /> {/* Pass the toggleEchoVisibility prop */}
+        <AppMenu toggleEchoVisibility={toggleEchoVisibility} />
         <ButtonWrapper title="ECHO" buttonImage={menu_white} setCurrentScreen={toggleEchoVisibility} />
       </div>
       {isEchoVisible && <Echo />}
