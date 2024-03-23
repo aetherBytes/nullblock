801630584af7c167f27c565c7fbb922fb8174b92
aetherBytes <sage.software42@gmail.com>
2024-03-23 09:02:02 -0600

    moar styles

--- svc/hecate/src/pages/home/index.tsx
+++ svc/hecate/src/pages/home/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Moxi from '@components/moxi/moxi'; // Adjust the import path as needed
-import Echo from '@components/echo/echo'; // Adjust the import path as needed
 import AppMenu from '@components/app-menu/app-menu'; // Adjust the import path as needed
+import styles_buttons from '@components/index-button-wrapper/styles.module.scss'; // Ensure the path is correct for your styles
+import ButtonWrapper from '@components/index-button-wrapper/button-wrapper'; // Adjust the import path as needed
 import styles from './index.module.scss'; // Ensure the path is correct for your styles
 import ChatInput from '@components/chat-input/chat-input'; // Ensure the import path is correct
 
