705910a1c5db3800e891ca6f77a4228190989bf8
aetherBytes <sage.software42@gmail.com>
2024-03-23 13:15:09 -0600

    button styles in sync

--- svc/hecate/src/common/components/button-wrapper/button-wrapper.tsx
+++ svc/hecate/src/common/components/button-wrapper/button-wrapper.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './button-wrapper.module.scss';
+import styles from './button-wrapper.module.scss'; // Correct path to your SCSS module
 
 const ButtonWrapper = ({ title, buttonImage, buttonText, setCurrentScreen }) => {
-  const handleClick = () => {
-    setCurrentScreen(); // Invoke the toggleMenu function
+  const createRipple = (event) => {
+    const circle = document.createElement("span");
+    const diameter = Math.max(event.currentTarget.clientWidth, event.currentTarget.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - event.currentTarget.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - event.currentTarget.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+
+    const rippleContainer = event.currentTarget;
+    rippleContainer.appendChild(circle);
+    setTimeout(() => {
+      circle.remove();
+    }, 600); // Adjust timing as needed
   };
 
   return (
-    <div>
-      <button type="button" className={styles.echoButton} onClick={handleClick}>
-        {buttonImage ? <img src={buttonImage} alt={title} className={styles.echoButtonImg} /> : buttonText}
+    <div className={styles.wrapper}> {/* Adjust className as needed */}
+      <button
+        type="button"
+        className={styles.powerButton} // Ensure this is the correct class for your button
+        onClick={(e) => {
+          setCurrentScreen();
+          createRipple(e);
+        }}
+      >
+        {buttonImage && <img src={buttonImage} alt={title} className={styles.echoButtonImg} />}
+        {buttonText}
       </button>
     </div>
   );
@@ -19,13 +40,8 @@ const ButtonWrapper = ({ title, buttonImage, buttonText, setCurrentScreen }) => 
 ButtonWrapper.propTypes = {
   title: PropTypes.string.isRequired,
   buttonText: PropTypes.string,
-  buttonImage: PropTypes.string, // Image source URL
+  buttonImage: PropTypes.string,
   setCurrentScreen: PropTypes.func.isRequired,
-};
-
-ButtonWrapper.defaultProps = {
-  buttonText: '',
-  buttonImage: '',
 };
 
 export default ButtonWrapper;
