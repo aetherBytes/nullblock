970eaca7a39f0f64d9b004fc88fec623b324bbf4
aetherBytes <sage.software42@gmail.com>
2024-04-06 06:41:36 -0600

    overlay

---
+++ svc/hecate/src/pages/agent-orange/index.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import AppMenu from '@components/app-menu/app-menu';
+import EchoChat from '@components/echo/echo-chat/echo-chat';
+import Echo from '@components/echo/echo';
+import ChatInput from '@components/chat-input/chat-input';
+import baseScreensConfig from '@components/echo/screens-config';
+import styles from './index.module.scss';
+import powerOn from '@assets/images/echo_bot.png';
+import powerOff from '@assets/images/echo_bot_black.png';
+
+const Home = () => {
+  const [isUIVisible, setIsUIVisible] = useState(false);
+  const [showEchoChat, setShowEchoChat] = useState(false);
+
+  const toggleUIVisibility = () => {
+    setIsUIVisible(!isUIVisible);
+    if (!isUIVisible) {
+      setShowEchoChat(false); // Resets to base Echo screen when turning off
+    }
+  };
+
+  const toggleToEchoChat = () => {
+    setShowEchoChat(true);
+  };
+
+  const closeEchoScreen = () => {
+    setShowEchoChat(false);
+  };
+
+  return (
+    <div className={styles.backgroundImage}>
+      <div className={styles.powerButtonContainer}> {/* Use a container class for the div */}
+        <button
+          onClick={toggleUIVisibility}
+          className={styles.powerButton} // Ensure the button has its own styles
+        >
+          <img src={isUIVisible ? powerOn : powerOff} alt="Power button" className={styles.powerButtonImage}/>
+          <span>{isUIVisible ? 'Deactivate' : 'Activate'}</span>
+        </button>
+      </div>
+      <div className={styles.bottomUIContainer}>
+        {isUIVisible && (
+          <>
+            <AppMenu toggleEchoVisibility={toggleToEchoChat} closeEchoScreen={closeEchoScreen} />
+            {showEchoChat ? <EchoChat /> : <Echo screensConfig={baseScreensConfig} defaultScreenKey="Nexus" />}
+            <ChatInput />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
+
+
+
