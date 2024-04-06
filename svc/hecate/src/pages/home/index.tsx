f973aa9fc23134062980866b3dbd52a0af84e564
aetherBytes <sage.software42@gmail.com>
2024-04-06 14:02:53 -0600

    save point

--- svc/hecate/src/pages/home/index.tsx
+++ svc/hecate/src/pages/home/index.tsx
@@ -1,16 +1,17 @@
+
 import React, { useState, useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
-import AppMenu from '@components/app-menu/app-menu';
+import AppMenu from '@components/echo-screens/home-screen/app-menu/app-menu';
 import EchoChat from '@components/echo-screens/home-screen/echo-chat/echo-chat';
 import Echo from '@components/echo-screens/home-screen/echo';
-import ChatInput from '@components/chat-input/chat-input';
+import ChatInput from '@components/echo-screens/home-screen/chat-input/chat-input';
 import baseScreensConfig from '@components/echo-screens/home-screen/screens-config';
 import styles from './index.module.scss';
 import powerOn from '@assets/images/echo_bot.png';
 import powerOff from '@assets/images/echo_bot_black.png';
 
 const Home = () => {
-  const [isUIVisible, setIsUIVisible] = useState(false);
+  const [isUIVisible, setIsUIVisible] = useState(true); // Default to true
   const [showEchoChat, setShowEchoChat] = useState(false);
 
   useEffect(() => {
@@ -32,7 +33,7 @@ const Home = () => {
   const toggleUIVisibility = () => {
     setIsUIVisible(!isUIVisible);
     if (!isUIVisible) {
-      setShowEchoChat(false); // Resets to base Echo screen when turning off
+      setShowEchoChat(false);
     }
   };
 
@@ -48,14 +49,13 @@ const Home = () => {
     <>
       <div className={styles.backgroundImage}></div>
       <div id="fogOverlay" className={styles.fogOverlay}></div>
-      {/* Spline object is positioned absolutely and on top of all other content */}
       <Spline className={styles.splineObject} scene="https://prod.spline.design/1Q-qMj7C6kFIofgB/scene.splinecode" />
-      <div className={styles.powerButtonContainer}> {/* Use a container class for the div */}
+      <div className={styles.powerButtonContainer}>
         <button
           onClick={toggleUIVisibility}
-          className={styles.powerButton} // Ensure the button has its own styles
+          className={styles.powerButton}
         >
-          <img src={isUIVisible ? powerOn : powerOff} alt="Power button" className={styles.powerButtonImage}/>
+          <img src={isUIVisible ? powerOn : powerOff} alt="Power button" className={styles.powerButtonImage} />
           <span>{isUIVisible ? 'Deactivate' : 'Activate'}</span>
         </button>
       </div>
@@ -63,7 +63,7 @@ const Home = () => {
         {isUIVisible && (
           <>
             <AppMenu toggleEchoVisibility={toggleToEchoChat} closeEchoScreen={closeEchoScreen} />
-            {showEchoChat ? <EchoChat /> : <Echo screensConfig={baseScreensConfig} defaultScreenKey="Nexus" />}
+            {showEchoChat && <EchoChat />}
             <ChatInput />
           </>
         )}
